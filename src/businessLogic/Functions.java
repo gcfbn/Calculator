3f9e2fd4f7eb06c7a97164b2aac5124896a04699
gcfbn <bmekarski@interia.pl>
2021-01-25 21:19:14 +0100

    Changed modifiers of methods.

--- src/businessLogic/Functions.java
+++ src/businessLogic/Functions.java
@@ -2,19 +2,19 @@ package businessLogic;
 
 public class Functions {
 
-    protected static String removeUnnecessaryDot(String argument) {
+    public static String removeUnnecessaryDot(String argument) {
 
         if (argument.charAt(argument.length() - 1) == '.') return argument.substring(0, argument.length() - 1);
         else return argument;
     }
 
-    protected static String removeUnnecessaryFractionalPart(double argument) {
+    public static String removeUnnecessaryFractionalPart(double argument) {
 
         if ((int) argument == argument) return Integer.toString((int) argument);
         else return Double.toString(argument);
     }
 
-    protected static String roundToNineDigits(String argument) {
+    public static String roundToNineDigits(String argument) {
 
         if (argument.length() > 9) return argument.substring(0, 9);
         else return argument;
